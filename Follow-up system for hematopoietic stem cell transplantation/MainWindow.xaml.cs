3a3f80ac63c5eb6c56c3b7de647fd70f9c2e00ef
LiPeterzette <88654584+LiPeterzette@users.noreply.github.com>
2023-04-12 18:46:09 +0800

    Add files via upload

---
+++ Follow-up system for hematopoietic stem cell transplantation/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+using ControlzEx.Theming;
+using CQ.Enterprise.App.AlgoPlatform.Applications;
+
+namespace CQ.Enterprise.App.AlgoPlatform
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ThemeManager.Current.ChangeTheme(this, "Light.Blue");
+        }
+
+        private void button_BigDataMgr_Clicked(object sender, RoutedEventArgs e)
+        {
+            StraSugWindow win1 = new StraSugWindow();
+            win1.Show();
+        }
+
+
+    }
+}
